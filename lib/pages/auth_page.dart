7a1aa2bde05232a1138070122ac0174731e31971
navas <gabrielnavas@email.com>
2024-03-18 15:31:03 -0300

    add loading page

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,5 +1,6 @@
 import 'package:chat_flutter_app/components/auth_form.dart';
 import 'package:chat_flutter_app/models/auth_form_data.dart';
+import 'package:chat_flutter_app/pages/loading_page.dart';
 import 'package:flutter/material.dart';
 
 class AuthPage extends StatefulWidget {
@@ -10,12 +11,19 @@ class AuthPage extends StatefulWidget {
 }
 
 class _AuthPageState extends State<AuthPage> {
+  bool _isLoading = false;
+
   void _handleSubmit(AuthFormData authFormData) {
+    setState(() => _isLoading = true);
     print(authFormData);
+    setState(() => _isLoading = false);
   }
 
   @override
   Widget build(BuildContext context) {
+    if (_isLoading) {
+      return const LoadingPage();
+    }
     return Scaffold(
       backgroundColor: Colors.blueAccent,
       body: Stack(
