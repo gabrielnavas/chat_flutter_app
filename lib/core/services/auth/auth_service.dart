3c4d234226296307ec459e952dd3fd60f33d91d8
navas <gabrielnavas@email.com>
2024-03-18 17:18:19 -0300

    create auth service abstract class

---
+++ lib/core/services/auth/auth_service.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+import 'package:chat_flutter_app/core/models/chat_user.dart';
+
+abstract class AuthService {
+  ChatUser? get currentUser;
+
+  Stream<ChatUser?> get userChanges;
+
+  Future<void> signup(
+    String name,
+    String email,
+    String password,
+    File image,
+  );
+
+  Future<void> signin(
+    String email,
+    String password,
+  );
+
+  Future<void> logout();
+}
