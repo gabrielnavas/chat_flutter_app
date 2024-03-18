9acdd6ef4a4c712f7e9c4736a99418ba1e55934b
navas <gabrielnavas@email.com>
2024-03-18 11:11:04 -0300

    loading page

---
+++ lib/pages/loading_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class LoadingPage extends StatelessWidget {
+  const LoadingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.blueAccent,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Carregando...',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            CircularProgressIndicator(
+              color: Colors.white,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
