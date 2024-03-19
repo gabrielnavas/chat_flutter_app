ace86898ffe601405ea1efc62fac04c85e8f7860
navas <gabrielnavas@email.com>
2024-03-18 21:11:07 -0300

    add dynamic animation with timer

--- lib/pages/loading_page.dart
+++ lib/pages/loading_page.dart
@@ -1,33 +1,58 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class LoadingPage extends StatelessWidget {
+class LoadingPage extends StatefulWidget {
   const LoadingPage({super.key});
 
   @override
+  State<LoadingPage> createState() => _LoadingPageState();
+}
+
+class _LoadingPageState extends State<LoadingPage> {
+  String dotResult = '';
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    _changeDots();
+
+    return Scaffold(
       backgroundColor: Colors.blueAccent,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              'Carregando...',
-              style: TextStyle(
+              "Carregando$dotResult",
+              style: const TextStyle(
                 color: Colors.white,
                 fontSize: 20,
                 fontWeight: FontWeight.w500,
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 10,
             ),
-            CircularProgressIndicator(
+            const CircularProgressIndicator(
               color: Colors.white,
             )
           ],
         ),
       ),
     );
+  }
+
+  void _changeDots() {
+    const dot = '.';
+
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        if (DateTime.now().second % 2 == 0) {
+          dotResult = '$dot$dot$dot';
+        } else {
+          dotResult = '$dot$dot';
+        }
+      });
+    });
   }
 }
