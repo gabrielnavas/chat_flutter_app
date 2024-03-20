110209533c1dbfee183a3b92798c618ec2ab9020
navas <gabrielnavas@email.com>
2024-03-19 21:32:55 -0300

    add drop down menu with logout item

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,8 +1,26 @@
+import 'package:chat_flutter_app/components/messages.dart';
+import 'package:chat_flutter_app/components/new_message.dart';
 import 'package:chat_flutter_app/core/services/auth/auth_service.dart';
 import 'package:flutter/material.dart';
 
+enum MenuItemValue { logout }
+
 class ChatPage extends StatelessWidget {
   const ChatPage({super.key});
+
+  void _showError(String error, BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(
+        error,
+        style: const TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 14,
+        ),
+      ),
+      backgroundColor: Colors.redAccent,
+    ));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,19 +28,57 @@ class ChatPage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.blueAccent,
         centerTitle: true,
+        actions: [
+          DropdownButton(
+            icon: const Icon(
+              Icons.more_vert,
+              color: Colors.white,
+            ),
+            items: const [
+              DropdownMenuItem(
+                value: MenuItemValue.logout,
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.exit_to_app,
+                      color: Colors.black87,
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Text(
+                      'Sair',
+                    ),
+                  ],
+                ),
+              )
+            ],
+            onChanged: (value) {
+              if (value == null) {
+                return;
+              }
+              switch (value) {
+                case MenuItemValue.logout:
+                  AuthService().logout().catchError(
+                        (error) => _showError(
+                          'Tente novamente mais tarde.',
+                          context,
+                        ),
+                      );
+              }
+            },
+          )
+        ],
         title: const Text(
           'Chat',
           style: TextStyle(color: Colors.white),
         ),
       ),
-      body: Center(
+      body: const SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('chat page'),
-            ElevatedButton(
-                onPressed: () => AuthService().logout(),
-                child: const Text('Logout'))
+            Expanded(child: Messages()),
+            NewMessage(),
           ],
         ),
       ),
