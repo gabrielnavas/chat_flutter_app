8160756dd8be6d857e279c60ae1049f41328f4e0
navas <gabrielnavas@email.com>
2024-03-19 23:41:48 -0300

    implements stream builder with list view on messages widget

--- lib/components/messages.dart
+++ lib/components/messages.dart
@@ -1,3 +1,5 @@
+import 'package:chat_flutter_app/core/models/chat_message.dart';
+import 'package:chat_flutter_app/core/services/chat/chat_service.dart';
 import 'package:flutter/material.dart';
 
 class Messages extends StatelessWidget {
@@ -5,6 +7,33 @@ class Messages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('messages');
+    return StreamBuilder(
+        stream: ChatService().messagesStream(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (!snapshot.hasData) {
+            return const Center(
+              child: Text('Nenhuma mensagem encontrada'),
+            );
+          }
+
+          final msgs = snapshot.data!;
+
+          return ListView.builder(
+            reverse: true,
+            itemCount: msgs.length,
+            itemBuilder: (ctx, index) {
+              final ChatMessage chatMessage = msgs[index];
+              return ListTile(
+                title: Text(
+                  chatMessage.userName,
+                  style: const TextStyle(backgroundColor: Colors.red),
+                ),
+                subtitle: Text('${chatMessage.createdAt}\n${chatMessage.text}'),
+              );
+            },
+          );
+        });
   }
 }
