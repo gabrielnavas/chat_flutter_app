e3f908529ed6500cae538dd9b2a611505adb62d1
navas <gabrielnavas@email.com>
2024-03-20 22:10:36 -0300

    refactory message_bubble

--- lib/components/message_bubble.dart
+++ lib/components/message_bubble.dart
@@ -1,5 +1,6 @@
 import 'package:chat_flutter_app/core/models/chat_message.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:intl/intl.dart';
 
 class MessageBubble extends StatelessWidget {
@@ -15,135 +16,68 @@ class MessageBubble extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     String messageFormatted = _formatText();
-    if (belongsToCurrentUser) {
-      return _renderIsCurrentUser(messageFormatted);
-    }
+    final BorderRadius borderCard = _borderCard();
 
-    return _renderIsNotCurrentUser(messageFormatted);
-  }
-
-  Widget _renderIsCurrentUser(String messageFormatted) {
-    BorderRadius border = const BorderRadius.only(
-      bottomLeft: Radius.circular(10),
-      topLeft: Radius.circular(10),
-      topRight: Radius.circular(10),
-    );
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 6, horizontal: 25),
-      alignment: Alignment.topRight,
-      child: Card(
-        elevation: 10,
-        shape: RoundedRectangleBorder(
-          borderRadius: border,
-        ),
-        child: Container(
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-          decoration: BoxDecoration(
-            color: Colors.blueAccent,
-            borderRadius: border,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                message.userName,
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
-                  color: Colors.white,
-                ),
+    return Stack(
+      children: [
+        Container(
+          margin: const EdgeInsets.symmetric(vertical: 6, horizontal: 25),
+          alignment: _alignmentContentStack(),
+          child: Card(
+            elevation: 10,
+            shape: RoundedRectangleBorder(
+              borderRadius: borderCard,
+            ),
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              decoration: _backgroundCard(borderCard),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    message.userName,
+                    style: _styleUserName(),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    messageFormatted,
+                    style: _styleUserMessage(),
+                  ),
+                  const SizedBox(
+                    height: 3,
+                  ),
+                  Text(
+                    textAlign: TextAlign.start,
+                    DateFormat('dd/MM/yyyy  H-m-s').format(message.createdAt),
+                    style: _styleUserCreatedAt(),
+                  )
+                ],
               ),
-              const SizedBox(
-                height: 10,
-              ),
-              Text(
-                messageFormatted,
-                style: const TextStyle(
-                  fontStyle: FontStyle.italic,
-                  fontSize: 15,
-                  color: Colors.white,
-                ),
-              ),
-              const SizedBox(
-                height: 3,
-              ),
-              Text(
-                textAlign: TextAlign.start,
-                DateFormat('dd/MM/yyyy  H-m-s').format(message.createdAt),
-                style: const TextStyle(
-                  fontStyle: FontStyle.italic,
-                  fontSize: 12,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.white60,
-                ),
-              )
-            ],
+            ),
           ),
         ),
-      ),
+        _userAvatarImage(
+          const CircleAvatar(
+            backgroundColor: Colors.pink,
+          ),
+        )
+      ],
     );
   }
 
-  Widget _renderIsNotCurrentUser(String messageFormatted) {
-    BorderRadius border = const BorderRadius.only(
-      topLeft: Radius.circular(10),
-      topRight: Radius.circular(10),
-    );
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 6, horizontal: 25),
-      alignment: Alignment.topLeft,
-      child: Card(
-        elevation: 5,
-        shape: RoundedRectangleBorder(
-          borderRadius: border,
-        ),
-        child: Container(
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-          decoration: BoxDecoration(
-            color: const Color.fromARGB(70, 0, 0, 0),
-            borderRadius: border,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                message.userName,
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
-                  color: Colors.black,
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Text(
-                messageFormatted,
-                style: const TextStyle(
-                  fontStyle: FontStyle.italic,
-                  fontSize: 15,
-                  color: Colors.black,
-                ),
-              ),
-              const SizedBox(
-                height: 3,
-              ),
-              Text(
-                textAlign: TextAlign.start,
-                DateFormat('dd/MM/yyyy  H-m-s').format(message.createdAt),
-                style: const TextStyle(
-                  fontStyle: FontStyle.italic,
-                  fontSize: 12,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.black54,
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
+  Positioned _userAvatarImage(CircleAvatar circleAvatar) {
+    return Positioned(
+      left: belongsToCurrentUser ? null : 10,
+      right: belongsToCurrentUser ? 20 : null,
+      top: belongsToCurrentUser ? 5 : null,
+      child: circleAvatar,
     );
   }
+
+  Alignment _alignmentContentStack() =>
+      belongsToCurrentUser ? Alignment.topRight : Alignment.topLeft;
 
   String _formatText() {
     int maxCharactersInLine = 35;
@@ -167,5 +101,76 @@ class MessageBubble extends StatelessWidget {
       }
     }
     return messageSplited;
+  }
+
+  TextStyle _styleUserCreatedAt() {
+    return belongsToCurrentUser
+        ? const TextStyle(
+            fontStyle: FontStyle.italic,
+            fontSize: 12,
+            fontWeight: FontWeight.w500,
+            color: Colors.white60,
+          )
+        : const TextStyle(
+            fontStyle: FontStyle.italic,
+            fontSize: 12,
+            fontWeight: FontWeight.w500,
+            color: Colors.black54,
+          );
+  }
+
+  TextStyle _styleUserName() {
+    return belongsToCurrentUser
+        ? const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+            color: Colors.white,
+          )
+        : const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+            color: Colors.black,
+          );
+  }
+
+  BoxDecoration _backgroundCard(BorderRadius borderCard) {
+    return belongsToCurrentUser
+        ? BoxDecoration(
+            color: Colors.blueAccent,
+            borderRadius: borderCard,
+          )
+        : BoxDecoration(
+            color: const Color.fromARGB(70, 0, 0, 0),
+            borderRadius: borderCard,
+          );
+  }
+
+  BorderRadius _borderCard() {
+    if (belongsToCurrentUser) {
+      return const BorderRadius.only(
+        bottomLeft: Radius.circular(10),
+        topLeft: Radius.circular(10),
+        topRight: Radius.circular(10),
+      );
+    }
+
+    return const BorderRadius.only(
+      topLeft: Radius.circular(10),
+      topRight: Radius.circular(10),
+    );
+  }
+
+  TextStyle _styleUserMessage() {
+    return belongsToCurrentUser
+        ? const TextStyle(
+            fontStyle: FontStyle.italic,
+            fontSize: 15,
+            color: Colors.white,
+          )
+        : const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+            color: Colors.black,
+          );
   }
 }
